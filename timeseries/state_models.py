d9f3d3c8e3def0d5e99192f0b2e1fa26a99b6af2
Christian Donner <chdonner@ethz.ch>
2021-06-07 12:30:10 +0000

    LSEM state model

--- timeseries/state_models.py
+++ timeseries/state_models.py
@@ -1,7 +1,7 @@
 import numpy
 import sys
 sys.path.append('../src/')
-import densities, conditionals
+import densities, conditionals, factors
 
 
 class StateModel:
@@ -223,6 +223,171 @@ class LinearStateModel(StateModel):
         opt_init_density = densities.GaussianDensity(Sigma0, mu0)
         return opt_init_density
         
+        
+class LSEMStateModel(LinearStateModel):
+    
+    def __init__(self, Dz: int, Dk: int, noise_z: float=1e-1):
+        """ This implements a linear+squared exponential mean (LSEM) state model
+        
+            z_t = A phi(z_{t-1}) + b + zeta_t     with      zeta_t ~ N(0,Qz).
+            
+            The feature function is 
+            
+            f(x) = (x_0, x_1,...,x_m, k(h_1(x))),...,k(h_n(x))).
+            
+            The kernel and linear activation function are given by
+            
+            k(h) = exp(-h^2 / 2) and h_i(x) = w_i'x + w_{i,0}.
+            
+            Hence, the 
+            
+        :param Dz: int
+            Dimensionality of latent space.
+        :param Dk: int
+            Number of kernels to use.
+        :param noise_z: float
+            Intial isoptropic std. on the state transition.
+        """
+        self.Dz, self.Dk = Dz, Dk
+        self.Dphi = self.Dk + self.Dz
+        self.Qz = noise_z ** 2 * numpy.eye(self.Dz)
+        self.A = numpy.zeros((self.Dz, self.Dphi))
+        self.A[:,:self.Dz] = numpy.eye(self.Dz)
+        self.b = numpy.zeros((self.Dz,))
+        self.W = 1e-4 * numpy.random.randn(self.Dk, self.Dz + 1)
+        self.state_density = conditionals.LSEMGaussianConditional(M=numpy.array([self.A]), 
+                                                                  b=numpy.array([self.b]), 
+                                                                  W=self.W, 
+                                                                  Sigma=numpy.array([self.Qz]))
+        
+    def update_hyperparameters(self, smoothing_density: 'GaussianDensity', 
+                               two_step_smoothing_density: 'GaussianDensity'):
+        """ The hyperparameters are updated here, where the the densities p(z_t|x_{1:T}) and 
+        p(z_{t+1}, z_t|x_{1:T}) are provided (the latter for the cross-terms.)
+        
+        :param smoothing_density: GaussianDensity
+            The smoothing density  p(z_t|x_{1:T}).
+        :param two_step_smoothing_density: Gaussian Density
+            The two point smoothing density  p(z_{t+1}, z_t|x_{1:T}).
+        """
+        self.update_A(smoothing_density, two_step_smoothing_density)
+        self.update_b(smoothing_density)
+        self.update_Qz(smoothing_density, two_step_smoothing_density)
+        self.update_W(smoothing_density, two_step_smoothing_density)
+        self.update_state_density()
+        
+    def update_A(self, smoothing_density: 'GaussianDensity', 
+                 two_step_smoothing_density: 'GaussianDensity'):
+        """ The transition matrix is updated here, where the the densities
+        p(z_{t+1}, z_t|x_{1:T}) is provided.
+        
+        :param smoothing_density: GaussianDensity
+            The smoothing density  p(z_t|x_{1:T}).
+        :param two_step_smoothing_density: Gaussian Density
+            The two point smoothing density  p(z_{t+1}, z_t|x_{1:T}).
+        """
+        T = two_step_smoothing_density.R
+        phi = smoothing_density.slice(range(T))
+        
+        # E[f(z)f(z)']
+        Ekk = phi.multiply(self.state_density.k_func).multiply(self.state_density.k_func).integrate().reshape((T, 
+                                                                                                               self.Dk, 
+                                                                                                               self.Dk))
+        Ekz = phi.multiply(self.state_density.k_func).integrate('x').reshape((T, 
+                                                                              self.Dk, 
+                                                                              self.Dz))
+        Eff = numpy.empty((self.Dphi, self.Dphi))
+        Eff[:self.Dz,:self.Dz] = numpy.sum(phi.integrate('xx'), axis=0)
+        Eff[self.Dz:,self.Dz:] = numpy.sum(Ekk, axis=0)
+        Eff[self.Dz:,:self.Dz] = numpy.sum(Ekz, axis=0)
+        Eff[:self.Dz,self.Dz:] = Eff[self.Dz:,:self.Dz].T
+        # E[f(z)] b'
+        Ez = numpy.sum(phi.integrate('x'), axis=0)
+        Ek = numpy.sum(phi.multiply(self.state_density.k_func).integrate().reshape((T,self.Dk)), axis=0)
+        Ef = numpy.concatenate([Ez, Ek])
+        Ebf = Ef[None] * self.b[:,None]
+        # E[z f(z)']
+        v_joint = numpy.zeros([self.Dk, int(2 * self.Dz)])
+        v_joint[:,self.Dz:] = self.state_density.k_func.v
+        nu_joint = numpy.zeros([self.Dk, int(2 * self.Dz)])
+        nu_joint[:,self.Dz:] = self.state_density.k_func.nu
+        ln_beta = self.state_density.k_func.ln_beta
+        joint_k_func = factors.OneRankFactor(v=v_joint, nu=nu_joint, ln_beta=ln_beta)
+        Ezz_cross = numpy.sum(two_step_smoothing_density.integrate('xx')[:,self.Dz:,:self.Dz], axis=0)
+        Ezk = numpy.sum(two_step_smoothing_density.multiply(joint_k_func).integrate('x').reshape((T, self.Dk, 
+                                                                                                 (2*self.Dz)))[:,:,:self.Dz], axis=0).T
+        Ezf = numpy.concatenate([Ezz_cross, Ezk], axis=1)
+        # This is added for stability. Could defined as prior. Check!
+        Eff += numpy.eye(self.Dphi)
+        self.A = numpy.linalg.solve(Eff, (Ezf -  Ebf).T).T
+        
+    def update_b(self, smoothing_density: 'GaussianDensity'):
+        """ The transition offset is updated here, where the the densities p(z_t|x_{1:T}) is provided.
+        
+        :param smoothing_density: GaussianDensity
+            The smoothing density  p(z_t|x_{1:T}).
+        """
+        T = smoothing_density.R - 1
+        Ez = smoothing_density.integrate('x')
+        Ek = smoothing_density.multiply(self.state_density.k_func).integrate().reshape((T+1,self.Dk))
+        Ef = numpy.concatenate([Ez, Ek], axis=1)
+        self.b = numpy.mean(smoothing_density.mu[1:] - numpy.dot(self.A, Ef[:-1].T).T, axis=0)
+        
+    def update_Qz(self, smoothing_density: 'GaussianDensity', two_step_smoothing_density: 'GaussianDensity'):
+        """ The transition covariance is updated here, where the the densities p(z_t|x_{1:T}) and 
+        p(z_{t+1}, z_t|x_{1:T}) are provided (the latter for the cross-terms.)
+        
+        :param smoothing_density: GaussianDensity
+            The smoothing density  p(z_t|x_{1:T}).
+        :param two_step_smoothing_density: Gaussian Density
+            The two point smoothing density  p(z_{t+1}, z_t|x_{1:T}).
+        """
+        T = smoothing_density.R - 1
+        Ezz = smoothing_density.integrate('xx')
+        # E[zz']
+        Ezz_sum = numpy.sum(Ezz[1:], axis=0)
+        # E[z f(z)'] A'
+        v_joint = numpy.zeros([self.Dk, int(2 * self.Dz)])
+        v_joint[:,self.Dz:] = self.state_density.k_func.v
+        nu_joint = numpy.zeros([self.Dk, int(2 * self.Dz)])
+        nu_joint[:,self.Dz:] = self.state_density.k_func.nu
+        joint_k_func = factors.OneRankFactor(v=v_joint, nu=nu_joint, ln_beta=self.state_density.k_func.ln_beta)
+        Ezz_cross = two_step_smoothing_density.integrate('xx')[:,self.Dz:,:self.Dz]
+        Ekz = two_step_smoothing_density.multiply(joint_k_func).integrate('x').reshape((T,self.Dk,int(2*self.Dz)))[:,:,:self.Dz]
+        Ezf = numpy.concatenate([Ezz_cross, numpy.swapaxes(Ekz,1,2)], axis=2)
+        EzfA = numpy.einsum('abc,dc->bd', Ezf, self.A)
+        # E[z]b'
+        Ezb = numpy.sum(smoothing_density.integrate('x')[1:,None] * self.b[None,:,None], axis=0)
+        # A E[f(z)] b'
+        Ez = numpy.sum(smoothing_density.integrate('x')[:-1], axis=0)
+        Ek = numpy.sum(smoothing_density.multiply(self.state_density.k_func).integrate().reshape((T+1,self.Dk))[:-1], axis=0)
+        Ef = numpy.concatenate([Ez, Ek])
+        AEfb = numpy.dot(self.A, Ef)[:,None] * self.b[None]
+        # A E[f(z)f(z)'] A'
+        Ekk = smoothing_density.multiply(self.state_density.k_func).multiply(self.state_density.k_func).integrate().reshape((T+1, self.Dk, self.Dk))
+        Ekz = smoothing_density.multiply(self.state_density.k_func).integrate('x').reshape((T+1, self.Dk, self.Dz))
+        Eff = numpy.empty((self.Dphi, self.Dphi))
+        Eff[:self.Dz,:self.Dz] = numpy.sum(Ezz[:-1], axis=0)
+        Eff[self.Dz:,self.Dz:] = numpy.sum(Ekk[:-1], axis=0)
+        Eff[self.Dz:,:self.Dz] = numpy.sum(Ekz[:-1], axis=0)
+        Eff[:self.Dz,self.Dz:] = Eff[self.Dz:,:self.Dz].T
+        AEffA = numpy.dot(numpy.dot(self.A, Eff), self.A.T)
+        self.Qz = (Ezz_sum - EzfA - EzfA.T + AEffA - Ezb - Ezb.T + AEfb + AEfb.T + T * self.b[:,None] * self.b[None]) / T
+        
+        
+    def update_W(self, smoothing_density: 'GaussianDensity', two_step_smoothing_density: 'GaussianDensity'):
+        pass
+    
+    def update_state_density(self):
+        """ Updates the state density.
+        """
+        self.state_density = conditionals.LSEMGaussianConditional(M=numpy.array([self.A]), 
+                                                                  b=numpy.array([self.b]), 
+                                                                  W=self.W, 
+                                                                  Sigma=numpy.array([self.Qz]))
+        self.Qz_inv, self.ln_det_Qz = self.state_density.Lambda[0], self.state_density.ln_det_Sigma[0]
+        
+    # TODO: Optimal initial state density
         
         
         
