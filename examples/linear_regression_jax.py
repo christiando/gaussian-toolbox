d8c45cadf4fc814c5e398a95c17e951b83be2216
christiando <christiandonner48@gmail.com>
2022-03-03 13:38:41 +0100

    made likelihood construction more intuitive

--- examples/linear_regression_jax.py
+++ examples/linear_regression_jax.py
@@ -1,16 +1,24 @@
 # %%
 import sys
-sys.path.append('../')
+
+sys.path.append("../")
 from jax import numpy as jnp
 import numpy as np
 from src_jax import factors, densities, conditionals
 from matplotlib import pyplot as plt
 from typing import Tuple
 
-class LinearRegression:
 
-    def __init__(self, X: jnp.ndarray, y: jnp.ndarray, sigma_y: float=.1,
-                 mu_prior: float = 0, sigma_prior: float = 1., bias: bool=True):
+class LinearRegression:
+    def __init__(
+        self,
+        X: jnp.ndarray,
+        y: jnp.ndarray,
+        sigma_y: float = 0.1,
+        mu_prior: float = 0,
+        sigma_prior: float = 1.0,
+        bias: bool = True,
+    ):
         """Linear regression class, i.e.
         
             y = Xw + b + \epsilon with \epsilon \sim N(0,\sigma_y)
@@ -35,7 +43,7 @@ class LinearRegression:
         if self.bias:
             self.D += 1
             self.X = jnp.ones((self.N, self.D))
-            self.X = self.X.at[:,1:].set(X)
+            self.X = self.X.at[:, 1:].set(X)
         else:
             self.X = X
         self.y = y
@@ -47,7 +55,7 @@ class LinearRegression:
     def _construct_prior(self):
         """Constructs the prior p(w\vert \mu_w, \sigma_w^2)
         """
-        Sigma = jnp.array([self.sigma_prior ** 2. * jnp.eye(self.D)])
+        Sigma = jnp.array([self.sigma_prior ** 2.0 * jnp.eye(self.D)])
         mu = self.mu_prior * jnp.ones((1, self.D))
         self.prior = densities.GaussianDiagDensity(Sigma=Sigma, mu=mu)
 
@@ -64,10 +72,18 @@ class LinearRegression:
         :rtype: factors.ConjugateFactor
         """
         Lambda = jnp.array([jnp.dot(X.T, X) / self.sigma_y ** 2.])
-        nu = jnp.array([jnp.dot(X.T, y) / self.sigma_y ** 2.])
+        nu = jnp.dot(y.T, X) / self.sigma_y ** 2.
+        print(nu.shape)
         ln_beta = jnp.array([- .5 * (jnp.dot(y.T, y) / self.sigma_y ** 2. +
-                          self.N * jnp.log(2. * jnp.pi * self.sigma_y ** 2.))])
+                         self.N * jnp.log(2. * jnp.pi * self.sigma_y ** 2.))])
         likelihood = factors.ConjugateFactor(Lambda=Lambda, nu=nu, ln_beta=ln_beta)
+        # Sigma_lk = jnp.ones((self.N, 1, 1)) * self.sigma_y ** 2
+        # M_lk = self.X[:, None]
+        # likelihood_term = conditionals.ConditionalGaussianDensity(
+        #     M=M_lk, Sigma=Sigma_lk
+        # )
+        # likelihood = likelihood_term.set_y(self.y)
+        # likelihood = likelihood.product()
         return likelihood
 
     def get_posterior(self):
@@ -104,16 +120,21 @@ class LinearRegression:
         N = X.shape[0]
         if self.bias:
             X_new = jnp.ones((N, self.D))
-            X_new = X_new.at[:,1:].set(X)
+            X_new = X_new.at[:, 1:].set(X)
         else:
             X_new = X
         M = jnp.reshape(X_new, (N, 1, X_new.shape[1]))
         b = jnp.zeros((N, 1))
-        Sigma = self.sigma_y ** 2. * jnp.ones((N, 1, 1))
-        likelihood_measure = conditionals.ConditionalGaussianDensity(M=M, b=b, Sigma=Sigma)
+        Sigma = self.sigma_y ** 2.0 * jnp.ones((N, 1, 1))
+        likelihood_measure = conditionals.ConditionalGaussianDensity(
+            M=M, b=b, Sigma=Sigma
+        )
         return likelihood_measure.affine_marginal_transformation(self.posterior)
 
-def generate_data(N: int, D: int, sigma_noise: float=.1, bias: bool=True) -> Tuple[jnp.ndarray]:
+
+def generate_data(
+    N: int, D: int, sigma_noise: float = 0.1, bias: bool = True
+) -> Tuple[jnp.ndarray]:
     """Generates data for linear regression.
 
     :param N: Number of data points
@@ -134,37 +155,45 @@ def generate_data(N: int, D: int, sigma_noise: float=.1, bias: bool=True) -> Tup
     else:
         b = jnp.zeros((1,))
     y = jnp.dot(X, w) + b + sigma_noise * jnp.array(np.random.randn(N,))
+    y = jnp.array([y]).T
     return X, y, w, b
 
+
 # %%
-if __name__=='__main__':
+if __name__ == "__main__":
     N = 20
     D = 1
     X, y, w, b = generate_data(N, D)
-    X_range = jnp.array([jnp.linspace(0,10,100)]).T
+    X_range = jnp.array([jnp.linspace(0, 10, 100)]).T
     lreg = LinearRegression(X, y)
     lreg.get_posterior()
     prediction_density = lreg.predict(X_range)
 
-    w_range, b_range = jnp.linspace(w[0] - .2, w[0] + .2, 100), jnp.linspace(b[0] - .2, b[0] + .2,100)
+    w_range, b_range = (
+        jnp.linspace(w[0] - 0.2, w[0] + 0.2, 100),
+        jnp.linspace(b[0] - 0.2, b[0] + 0.2, 100),
+    )
     w_mesh, b_mesh = jnp.meshgrid(w_range, b_range)
     mesh = jnp.vstack([b_mesh.flatten(), w_mesh.flatten()]).T
     posterior_mesh = lreg.posterior.evaluate(mesh)
-    plt.figure(figsize=(8,4))
+    plt.figure(figsize=(8, 4))
     plt.subplot(121)
-    plt.pcolor(b_range, w_range, posterior_mesh.reshape(100,100))
-    plt.plot(b, w, 'C3o')
-    plt.xlabel('Bias')
-    plt.ylabel('Weight')
-    plt.title('Posterior')
+    plt.pcolor(b_range, w_range, posterior_mesh.reshape(100, 100))
+    plt.plot(b, w, "C3o")
+    plt.xlabel("Bias")
+    plt.ylabel("Weight")
+    plt.title("Posterior")
     plt.subplot(122)
-    plt.plot(X, y, 'k.')
-    mu, std = jnp.squeeze(prediction_density.mu), jnp.sqrt(jnp.squeeze(prediction_density.Sigma))
-    plt.fill_between(jnp.squeeze(X_range), mu - std, mu + std, alpha=.5)
-    plt.plot(jnp.squeeze(X_range), mu, 'C0')
-    plt.plot(jnp.squeeze(X_range), jnp.dot(X_range, w) + b, 'k--')
-    plt.xlabel('X')
-    plt.ylabel('y')
-    plt.title('Data space')
+    plt.plot(X, y, "k.")
+    mu, std = (
+        jnp.squeeze(prediction_density.mu),
+        jnp.sqrt(jnp.squeeze(prediction_density.Sigma)),
+    )
+    plt.fill_between(jnp.squeeze(X_range), mu - std, mu + std, alpha=0.5)
+    plt.plot(jnp.squeeze(X_range), mu, "C0")
+    plt.plot(jnp.squeeze(X_range), jnp.dot(X_range, w) + b, "k--")
+    plt.xlabel("X")
+    plt.ylabel("y")
+    plt.title("Data space")
     plt.show()
 # %%
