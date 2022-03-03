849459b489db9f083f803620e3923619aca1e30e
christiando <christiandonner48@gmail.com>
2022-03-03 13:30:38 +0100

    formatted

--- tests/src_jax/test_densities.py
+++ tests/src_jax/test_densities.py
@@ -9,11 +9,11 @@ from jax import numpy as jnp
 from jax import scipy as jsc
 import numpy as np
 
+
 class TestGaussianDensity:
-    
     def setup_class(self):
         self.test_class = densities.GaussianDensity
-        
+
     @pytest.mark.parametrize("R, D", [(100, 5), (1, 5), (100, 1)])
     def test_init(self, R, D):
         Sigma = jnp.zeros((R, D, D))
@@ -30,7 +30,7 @@ class TestGaussianDensity:
         mu = None
         with pytest.raises(TypeError):
             d = self.test_class(Sigma, mu)
-            
+
     @pytest.mark.parametrize("R, D", [(100, 5), (1, 5), (100, 1)])
     def test_sample(self, R, D):
         np.random.seed(0)
@@ -45,7 +45,7 @@ class TestGaussianDensity:
         std = jnp.sqrt(jnp.diagonal(Sigma, axis1=1, axis2=2))
         assert jnp.allclose(mu_sample, mu, atol=1e-2)
         assert jnp.allclose(std_sample, std, atol=1e-2)
-        
+
     @pytest.mark.parametrize(
         "R, D, idx",
         [
@@ -72,7 +72,7 @@ class TestGaussianDensity:
         assert jnp.alltrue(d_new.ln_beta == d.ln_beta[idx])
         assert jnp.alltrue(d_new.ln_det_Sigma == d.ln_det_Sigma[idx])
         assert jnp.alltrue(d_new.ln_det_Lambda == d.ln_det_Lambda[idx])
-        
+
     @pytest.mark.parametrize(
         "R, D, dim_x",
         [
@@ -88,13 +88,13 @@ class TestGaussianDensity:
         mu = jnp.asarray(np.random.randn(R, D))
         d = self.test_class(Sigma, mu)
         md = d.get_marginal(dim_x)
-        assert jnp.alltrue(md.mu == mu[:,dim_x])
+        assert jnp.alltrue(md.mu == mu[:, dim_x])
         idx = jnp.ix_(jnp.arange(R), dim_x, dim_x)
         assert jnp.alltrue(md.Sigma == Sigma[idx])
         assert jnp.alltrue(md.is_normalized())
         assert jnp.alltrue(md.integrate() == 1)
-        
-    @pytest.mark.parametrize(    
+
+    @pytest.mark.parametrize(
         "R, D, dim_y",
         [
             (100, 5, jnp.array([0, 1, 2])),
@@ -108,34 +108,33 @@ class TestGaussianDensity:
         Sigma = jnp.zeros((R, D, D))
         rand_nums = np.random.rand(R)
         Sigma = Sigma.at[:].set(jnp.eye(D)[None] * rand_nums[:, None, None])
-        Lambda = jnp.eye(D)[None] * 1./rand_nums[:, None, None]
+        Lambda = jnp.eye(D)[None] * 1.0 / rand_nums[:, None, None]
         idx_x = jnp.ix_(jnp.arange(R), dim_x, dim_x)
         idx_y = jnp.ix_(jnp.arange(R), dim_y, dim_y)
         mu = jnp.asarray(np.random.randn(R, D))
         d = self.test_class(Sigma, mu)
-        M_x = -jnp.einsum("abc,acd->abd", Sigma[idx_x], Lambda[:, dim_x][:,:,dim_y])
+        M_x = -jnp.einsum("abc,acd->abd", Sigma[idx_x], Lambda[:, dim_x][:, :, dim_y])
         b_x = mu[:, dim_x] - jnp.einsum("abc,ac->ab", M_x, mu[:, dim_y])
         cd = d.condition_on(dim_y)
         assert jnp.alltrue(cd.Lambda == d.Lambda[idx_x])
         assert jnp.alltrue(cd.M == M_x)
         assert jnp.alltrue(cd.b == b_x)
-        
-    @pytest.mark.parametrize("R, D", [(100, 5), (1, 5), (100, 1)])   
+
+    @pytest.mark.parametrize("R, D", [(100, 5), (1, 5), (100, 1)])
     def test_to_dict(self, R, D):
         Sigma = jnp.zeros((R, D, D))
-        rand_nums = np.random.rand(R) + .1
+        rand_nums = np.random.rand(R) + 0.1
         Sigma = Sigma.at[:].set(jnp.eye(D)[None] * rand_nums[:, None, None])
-        Lambda = jnp.eye(D)[None] * 1./rand_nums[:, None, None]
+        Lambda = jnp.eye(D)[None] * 1.0 / rand_nums[:, None, None]
         mu = jnp.asarray(np.random.randn(R, D))
         d = self.test_class(Sigma, mu)
         d_dict = d.to_dict()
-        assert jnp.alltrue(d_dict['Sigma'] == Sigma)
-        assert jnp.alltrue(d_dict['mu'] == mu)
-        assert jnp.allclose(d_dict['Lambda'], Lambda)
-        assert jnp.allclose(d_dict['ln_det_Sigma'], jnp.sum(jnp.log(jnp.diagonal(Sigma, axis1=1, axis2=2)), axis=1), atol=1e-6)
-        
-    
-        
-        
+        assert jnp.alltrue(d_dict["Sigma"] == Sigma)
+        assert jnp.alltrue(d_dict["mu"] == mu)
+        assert jnp.allclose(d_dict["Lambda"], Lambda)
+        assert jnp.allclose(
+            d_dict["ln_det_Sigma"],
+            jnp.sum(jnp.log(jnp.diagonal(Sigma, axis1=1, axis2=2)), axis=1),
+            atol=1e-6,
+        )
 
-        
