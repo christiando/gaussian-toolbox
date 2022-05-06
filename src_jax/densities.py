cb135acdeba5d1d4e43be8ead9d1deabdb1105e5
christiando <christiandonner48@gmail.com>
2022-05-06 13:29:26 +0200

    fix: TypeError if sigma and mu not properly specified

--- src_jax/densities.py
+++ src_jax/densities.py
@@ -96,6 +96,8 @@ class GaussianDensity(measures.GaussianMeasure):
         :param ln_det_Sigma: jnp.ndarray [R] or None
             Log determinant of the covariance matrix. (Default=None)
         """
+        if not isinstance(Sigma, jnp.DeviceArray) or not isinstance(mu, jnp.DeviceArray):
+            raise TypeError("Sigma not properly specified")
         if Lambda is None:
             Lambda, ln_det_Sigma = invert_matrix(Sigma)
         elif ln_det_Sigma is None:
