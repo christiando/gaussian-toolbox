a1c57b7e54252803d7779baa4972f2e72f8a4452
Christian Donner <chdonner@ethz.ch>
2022-12-30 15:28:32 +0100

    test:hcov tests

--- tests/test_approximate_conditional.py
+++ tests/test_approximate_conditional.py
@@ -12,6 +12,7 @@ from jax import config
 import jax
 
 config.update("jax_enable_x64", True)
+config.update("jax_debug_nans", True)
 import numpy as np
 import objax
 
@@ -105,3 +106,77 @@ class TestLSEMGaussianConditional(TestLRBFGaussianConditional):
         Sigma_x = self.get_pd_matrix(R, Dx)
         p_X = pdf.GaussianPDF(Sigma=Sigma_x, mu=mu_x)
         return cond, p_X
+    
+class TestHCCovGaussianConditional:
+    
+    @classmethod
+    def create_instance(self, R, Dx, Dy, Du):
+        noise_x = 1.
+        C = jnp.array(np.random.randn(Dy, Dx))
+        C = C / jnp.sqrt(jnp.sum(C**2, axis=0))[None]
+        d = 1e-1 * jnp.array(np.random.randn(Dy))
+        rand_mat = np.random.rand(Dy, Dy) - 0.5
+        Q, _ = np.linalg.qr(rand_mat)
+        U = jnp.array(Q[:, : Du])
+        # self.U = jnp.eye(Dx)[:, :Du]
+        W = np.random.randn(Du, Dx + 1)
+        W[:, 0] = 0
+        W = jnp.array(W)
+        beta = .5**2 * jnp.ones(Du)
+        sigma_x = jnp.array([noise_x])
+        cond = approximate_conditional.HCCovGaussianConditional(
+            M=jnp.array([C]),
+            b=jnp.array([d]),
+            sigma_x=sigma_x,
+            U=U,
+            W=W,
+            beta=beta,
+        )
+        mu_x = 0 * objax.random.normal((R, Dx))
+        Sigma_x = 10 * self.get_pd_matrix(R, Dx)#jnp.tile(jnp.eye(Dx)[None], (R, 1, 1))#
+        p_X = pdf.GaussianPDF(Sigma=Sigma_x, mu=mu_x)
+        return cond, p_X
+    
+    
+    @staticmethod
+    def get_pd_matrix(R, D, eigen_mu=1):
+        # Q = objax.random.normal((R, D, D))
+        # eig_vals = jnp.abs(eigen_mu + objax.random.normal((R, D)))
+        # psd_mat = jnp.einsum("abc,abd->acd", Q * eig_vals[:, :, None], Q)
+        # psd_mat = 0.5 * (psd_mat + jnp.swapaxes(psd_mat, -1, -2))
+        A = objax.random.uniform((R, D, D))
+        psd_mat = jnp.einsum("abc,abd->acd", A, A)
+        psd_mat += jnp.eye(D)[None]
+        return psd_mat
+    
+    @pytest.mark.parametrize(
+        "R, Dx, Dy, Du", [(1, 5, 2, 2), (1, 10, 3, 1), (1, 2, 5, 5),],
+    )
+    def test_affine_tranformations(self, R, Dx, Dy, Du):
+        cond, p_X = self.create_instance(R, Dx, Dy, Du)
+        p_YX = cond.affine_joint_transformation(p_X)
+        p_Y = cond.affine_marginal_transformation(p_X)
+        p_X_given_Y = cond.affine_conditional_transformation(p_X)
+        dim_y = jnp.arange(Dx, Dy + Dx)
+        p_Y2 = p_YX.get_marginal(dim_y)
+        assert jnp.allclose(p_Y.mu, p_Y2.mu)
+        assert jnp.allclose(p_Y.Sigma, p_Y2.Sigma)
+        assert jnp.allclose(p_Y.Lambda, p_Y2.Lambda)
+        p_X_given_Y2 = p_YX.condition_on(dim_y)
+        assert jnp.allclose(p_X_given_Y.M, p_X_given_Y2.M)
+        assert jnp.allclose(p_X_given_Y.b, p_X_given_Y2.b)
+        assert jnp.allclose(p_X_given_Y.Sigma, p_X_given_Y2.Sigma)
+        assert jnp.allclose(p_X_given_Y.Lambda, p_X_given_Y2.Lambda)
+        
+    @pytest.mark.parametrize(
+        "R, Dx, Dy, Du", [(1, 5, 1, 1), (1, 10, 1, 1), (1, 5, 2, 2), (1, 1, 10, 2)],
+    )
+    def test_integrate_log_conditional_y(self, R, Dx, Dy, Du):
+        N = 1
+        y = jnp.array(np.random.randn(N, Dy))
+        cond, p_X = self.create_instance(R, Dx, Dy, Du)
+        integral_lb = cond.integrate_log_conditional_y(p_X, y=y)
+        key = jax.random.PRNGKey(42)
+        X_sample = p_X.sample(key, 10000)
+        integral_sample = jnp.mean(cond(X_sample[:,0]).evaluate_ln(y), axis=0)
+        assert jnp.all(integral_lb <= integral_sample)
