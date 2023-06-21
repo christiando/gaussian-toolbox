4b5b7462e913d2a86a84ac04f3ea510b9c13fc80
Christian Donner <chdonner@ethz.ch>
2023-06-21 17:24:44 +0200

    chore:restructuring of approximate condtionals

--- gaussian_toolbox/__init__.py
+++ gaussian_toolbox/__init__.py
@@ -1,9 +1,9 @@
-from . import factor, measure, pdf, conditional, approximate_conditional
+from . import factor, heteroscedastic_conditional, measure, pdf, conditional
 
 __all__ = [
     "factor",
     "measure",
     "pdf",
     "conditional",
-    "approximate_conditional",
+    "approximate_conditional" "heteroscedastic_conditional",
 ]
